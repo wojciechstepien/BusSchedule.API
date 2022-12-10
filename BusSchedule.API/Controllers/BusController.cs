f9c93d4c1463d4763dade3ba9d15eee2158ccf87
Wojciech <wojciech.tomasz.stepien@gmail.com>
2022-12-10 16:43:37 +0100

    added 406 not suported response if asked for unsuported response type added xml serializer formatter

--- BusSchedule.API/Controllers/BusController.cs
+++ BusSchedule.API/Controllers/BusController.cs
@@ -4,20 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BusSchedule.API.Controllers
 {
-    [Route("api/stops/{id}/bus")]
-    [Route("api/bus")]
+    [Route("api/stops/{stopId}/bus")]
     [ApiController]
     public class BusController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IEnumerable<BusDto>> GetStopBus(int id)
+        public ActionResult<IEnumerable<BusDto>> GetStopBuses(int stopId)
         {
-            var buses = StopsDataStore.Current.Stops.FirstOrDefault(s => s.Id == id);
-            if(buses?.BusList == null)
+            var buses = StopsDataStore.Current.Stops.FirstOrDefault(s => s.Id == stopId);
+            if (buses?.BusList == null)
             {
                 return NotFound();
             }
             return Ok(buses.BusList);
+        }
+
+        [HttpGet("{busid}")]
+        public ActionResult<BusDto> GetStopBus(int stopId,int busid)
+        {
+            var buses = StopsDataStore.Current.Stops.FirstOrDefault(s => s.Id == stopId);
+            if (buses?.BusList == null)
+            {
+                return NotFound();
+            }
+            var stopbus = buses.BusList.FirstOrDefault(s => s.Id == busid);
+            if (stopbus == null)
+            {
+                return NotFound();
+            }
+            return Ok(stopbus);
         }
     }
 }
