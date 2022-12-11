6d0c0af2c549be084911d5ca07f8878815172393
Wojciech <wojciech.tomasz.stepien@gmail.com>
2022-12-11 19:48:07 +0100

    Post request to add new stop

--- BusSchedule.API/Controllers/StopsController.cs
+++ BusSchedule.API/Controllers/StopsController.cs
@@ -14,7 +14,7 @@ namespace BusSchedule.API.Controllers
         }
 
 
-        [HttpGet("{stopId}")]
+        [HttpGet("{stopId}", Name = "GetStop")]
         public ActionResult<StopDto> GetStop(int stopId) 
         {
             var stopToReturn = StopsDataStore.Instance.Stops.FirstOrDefault(c => c.Id == stopId);
@@ -23,6 +23,20 @@ namespace BusSchedule.API.Controllers
                 return NotFound();
             }
             return Ok(stopToReturn);
+        }
+        [HttpPost]
+        public ActionResult<StopDto> CreateStop(string Name)
+        {
+            var stops = StopsDataStore.Instance.Stops;
+            var stop = StopsDataStore.Instance.Stops.FirstOrDefault(s => s.Name == Name);
+            if (stop == null)
+            {
+                var maxId = StopsDataStore.Instance.Stops.Max(c => c.Id);
+                var newStop = new StopDto() { Id = ++maxId,Name = Name };
+                stops.Add(newStop);
+                return CreatedAtRoute("GetStop",new {stopId = newStop.Id},newStop);
+            }
+            return NotFound();
         }
     }
 }
